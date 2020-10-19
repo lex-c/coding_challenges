fbe6e942b4b344272a7bd241cc7855f77ad5d0a1
lex-c <chlochlo.lexa@gmail.com>
2020-10-19 10:56:37 -0400

    add

---
+++ scratch.js
@@ -0,0 +1,23 @@
+
+
+const fs = require('fs')
+// console.log(fs)
+
+const output = fs.readFileSync('data.txt', 'utf8')
+    .trim()
+    .split('\n')
+    .map(line => line.split('\t'))
+    .reduce((customers, line) => {
+        customers[line[0]] = customers[line[0]] || []
+        customers[line[0]].push({
+            name: line[1],
+            price: line[2],
+            quantity: line[3]
+        })
+        return customers
+    },{})
+
+
+  
+    
+    console.log(JSON.stringify(output, (el) =>, 2))
